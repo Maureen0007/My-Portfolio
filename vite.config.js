ab6314bee1fea410c9c0a748b7af48f821a80d4d
Maureen <maureenwanjiku85@outlook.com>
2025-03-24 19:01:34 +0300

    install gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  base: "https://github.com/Maureen0007/My-Portfolio", // Replace with your GitHub repo name
+});
