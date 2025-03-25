215db9bb4a6ab00f3a0be3c5c16340221bf05414
Maureen <maureenwanjiku85@outlook.com>
2025-03-25 15:36:33 +0300

    Adjust Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState} from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Projects from "./pages/Projects";
@@ -9,16 +9,17 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router>
-    <Navbar /> 
-    <Routes>
-      <Route path="/" element={<Hero />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </Router>
+  <>
+      
+      <HashRouter>
+      <Navbar />
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </HashRouter>
+    </>
   );
 }
 
